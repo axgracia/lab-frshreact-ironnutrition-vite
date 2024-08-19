8f9826f07537d94a39fba7742f8fd6867b01de8f
axartmiami@gmail.com <axartmiami@gmail.com>
2024-08-18 23:41:48 -0400

    created app and foodbox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index}>{food.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,20 @@
-// Your code here
+
+
+function FoodBox({ food }) {
+    const { name, calories, image, servings } = food;
+  
+    return (
+      <div>
+        <p>{name}</p>
+        <img src={image} alt={name} />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>Total Calories: {servings * calories}</b> kcal
+        </p>
+        <button>Delete</button>
+      </div>
+    );
+  }
+  
+  export default FoodBox;
